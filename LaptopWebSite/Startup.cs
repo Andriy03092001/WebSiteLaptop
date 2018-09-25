5d99a1335e40357ead8dd9f544a51372de2e26fc
Andriy Riabuy <cuanid236316@gmail.com>
2018-09-25 15:24:51 +0300

    first commit

---
+++ LaptopWebSite/Startup.cs
@@ -0,0 +1,16 @@
+﻿using LaptopWebSite.Controllers;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaptopWebSite.Startup))]
+namespace LaptopWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+          
+        }
+    }
+}
